df0cca8f048fed3ddb4d16e36815d7216e499716
Jessica Kelly <JessKelly91@outlook.com>
2025-04-16 17:55:36 -0400

    Add PolicyTrack DB Context using EntityFramework

---
+++ PolicyTrack.API/Data/PolicyTrackContext.cs
@@ -0,0 +1,18 @@
+﻿using PolicyTrack.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PolicyTrack.API.Data
+{
+    public class PolicyTrackContext : DbContext
+    {
+        public PolicyTrackContext(DbContextOptions<PolicyTrackContext> options)
+            : base(options)
+        { }
+
+        public DbSet<PolicyHolder> PolicyHolders { get; set; }
+        public DbSet<Policy> Policies { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- PolicyTrack.API/Program.cs
+++ PolicyTrack.API/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using PolicyTrack.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<PolicyTrackContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
